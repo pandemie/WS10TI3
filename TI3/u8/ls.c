552e335c0becd163dc4f361724d89928381ea422
Dimitri Schachmann <d.schachmann@gmail.com>
2011-01-06 21:53:36 +0100

    so zeug

--- TI3/u8/ls.c
+++ TI3/u8/ls.c
@@ -1,82 +1,134 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "helper.h"
 
-short getSectorSize(FILE * myFile);
-char  getClusterSize(FILE * myFile);
-short getReservedSize(FILE * myFile);
-char  getFATcount(FILE * myFile);
-short getRootSize(FILE * myFile);
-int   getFSsize(FILE * myFile);
-short getFATsize(FILE * myFile);
+#define MAX_FOLDER_DEPTH 64
+
+//short getSectorSize(FILE * myFile);
+//char  getClusterSize(FILE * myFile);
+//short getReservedSize(FILE * myFile);
+//char  getFATcount(FILE * myFile);
+//short getRootSize(FILE * myFile);
+//int   getFSsize(FILE * myFile);
+//short getFATsize(FILE * myFile);
+//int   getFileStartSector(int x, FILE * myFile);
+//int   getRootSector(FILE * myFile);
+//void  printDir(char* path, FILE * myFile);
+//int getClusterAdress(int number, FILE * f);
+void printDir(char* path, FILE * f);
+void printDirAt(int adress, FILE * f);
+int  getFolderAdress(char* path, FILE * f);
 
 int main(int argc, char* argv){
   
   FILE * myFile = fopen("drive.img","r");
-  printf("%d\n",getSectorSize(myFile));
-  printf("%d\n",getClusterSize(myFile));
-  printf("%d\n",getReservedSize(myFile));
-  printf("%d\n",getFATcount(myFile));
-  printf("%d\n",getRootSize(myFile));
-  printf("%d\n",getFSsize(myFile));
-  printf("%d\n",getFATsize(myFile));
+  printf("sector size: %d\n",getSectorSize(myFile));
+  printf("sectors per cluster: %d\n",getClusterSize(myFile));
+  printf("reserved sectors: %d\n",getReservedSize(myFile));
+  printf("FATcount: %d\n",getFATcount(myFile));
+  printf("RootSize: %d\n",getRootSize(myFile));
+  printf("FSsize in sectors: %d\n",getFSsize(myFile));
+  printf("FATsize: %d\n",getFATsize(myFile));
+  printf("3. Cluster: %d\n",getFileStartSector(3,myFile));
+  printf("Adress of 3. Cluster: %x\n",getFileStartSector(0,myFile)*getSectorSize(myFile));
+  printf("Sector of root dir: %x\n",getRootSector(myFile));
+  printf("adress of Sector of root dir: %x\n",getRootSector(myFile)*getSectorSize(myFile));
+  //getFolderAdress("/IF/HALLO",myFile);
+  //printDir("/",myFile);
+  int b = 3;
+  printf("cluster %i adress: %x\n",b,getClusterAdress(b,myFile));
+  printDirAt(getClusterAdress(b,myFile),myFile);
+
   return 0;
 }
 
-//returns the number of bytes in a sector
-short getSectorSize(FILE * myFile){
-  short s;
-  fseek(myFile, 11, SEEK_SET);
-  fread(&s,2,1,myFile);
-  return s;
-}
-
-//returns the number  of sectors in a cluster
-char getClusterSize(FILE * myFile){
-  char c;
-  fseek(myFile, 13, SEEK_SET);
-  fread(&c,1,1,myFile);
-  return c;
-}
-
-//number of reserved sectors
-short getReservedSize(FILE * myFile){
-  short s;
-  fseek(myFile,14, SEEK_SET);
-  fread(&s,2,1,myFile);
-  return s;
-}
-
-//nuber of FATables
-char  getFATcount(FILE * myFile){
-  char c;
-  fseek(myFile, 16, SEEK_SET);
-  fread(&c,1,1,myFile);
-  return c;
-}
-
-//returns maximum number of files in root directory
-short  getRootSize(FILE * myFile){
-  short s;
-  fseek(myFile, 17, SEEK_SET);
-  fread(&s,2,1,myFile);
-  return s;
-}
-
-//returns the size of the filesystem in sectors
-int getFSsize(FILE * myFile){
-  int i = 0;
-  fseek(myFile, 19, SEEK_SET);
-  fread(&i,2,1,myFile);
-  if(0 == i){
-    fseek(myFile, 32, SEEK_SET);
-    fread(&i,4,1,myFile);
+//prints the contens of a directory
+void printDir(char* path, FILE * f){
+  if(strcmp(path,"/") == 0){
+    int adress = getRootSector(f)*getSectorSize(f);
+    printDirAt(adress,f);
+  } else {
+    char* dirs[11*MAX_FOLDER_DEPTH];
+    
   }
-  return i;
+  
 }
 
-//size of a FAT in sectors
-short getFATsize(FILE * myFile){
-  short s;
-  fseek(myFile, 22, SEEK_SET);
-  fread(&s,2,1,myFile);
-  return s;
+int getFolderAdress(char* path, FILE * f ){
+  if(*path != '/'){
+    printf("no slash\n");
+  }
+  ++path;
+  char* current = path;
+  char type;
+  char c;
+  int adress = getRootSector(f)*getSectorSize(f);
+  fseek(f,adress+11, SEEK_SET);
+  fread(&type,1,1,f);
+    
+  while(type != 0){
+    //directory
+    if(type == 0x10 && checkDir(path, adress)){
+      adress = adress+26;
+      fseek(f,adress+11, SEEK_SET);
+      fread(&type,1,1,f);
+      while(0){
+	if(*path == '/'){
+	  ++path;
+	  break;
+	}else{
+	  ++path;
+	}
+      }
+    } else {
+      adress = adress+0x20;
+      fseek(f,adress+11, SEEK_SET);
+      fread(&type,1,1,f);
+    }
+  }    
+}
+
+
+
+void printDirAt(int adress, FILE * f){
+  
+  char type;
+  char c;
+  fseek(f,adress+11, SEEK_SET);
+  fread(&type,1,1,f);
+    
+  while(type != 0){
+
+    //directory
+    if(type == 0x10){
+      for(int i = 0; i<11;++i){
+	fseek(f, adress+i, SEEK_SET);
+	fread(&c,1,1,f);
+	  
+	if(c == ' '){
+	  break;
+	} else {
+	  printf("%c",c);
+	}
+	  
+      }
+      printf("<dir>\n");
+    }
+    adress = adress+0x20;
+    fseek(f,adress+11, SEEK_SET);
+    fread(&type,1,1,f);      
+  }    
+}
+
+int checkDir(char* path, char* dirname){
+  for(int i = 0;i<11;++i){
+    if((*path == '/' || *path == 0) && (*dirname == ' ' || *dirname == 0x10)){
+      return 0;
+    } else if(*path != *dirname){
+      return 1;
+    }
+    ++path;
+    ++dirname;
+  }
 }
