f5b2510b1a55a086e0c73f9175be3b0217ec54f9
Dimitri Schachmann <d.schachmann@gmail.com>
2011-01-20 13:03:00 +0100

    verschlimmbessert

--- TI3/u10/crc.c
+++ TI3/u10/crc.c
@@ -1,5 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#define BUFSIZE         1024
+#define PATHSIZE        256
+#define TRASHFOLDER ".ti3_trash"
+#define COPY_SUCCESS        0
+#define COPY_FILE_EXISTS    1
+#define COPY_NO_SOURCE      2
+#define COPY_ERROR          3
+
+char copy_buffer[BUFSIZE];
 
 //bit operations on a char array
 #define ISBITSET(x,i) ((x[i>>3] & (1<<(i&7)))!=0)
@@ -12,6 +30,8 @@ void clearbit(short* x,int i);
 int isbitset(short* x,int i);
 void printBinArray(char* ar, int len,int start_bit);
 void computeCRC(char* msg,int msg_len, char* poly, int poly_len);
+int copyPLUScrc(char *source, char *target);
+int copyMINUScrc(char *source, char *target);
 
 short tmp = 0;
 short reg = 0;
@@ -22,6 +42,8 @@ char crc[2] = {0,0};
 
 int main(int argc, char* argv){
 
+
+  
   //set the polynom. reverse order because of endianness
   SETBIT(poly,0);//bit 16
   SETBIT(poly,1);//bit 15
@@ -33,6 +55,8 @@ int main(int argc, char* argv){
   
   computeCRC(msg,1*8,poly,p_len);
   printBinArray(crc,16,0);
+  //copyPLUScrc("test","./test.crc");
+  copyMINUScrc("test.crc","./test");
   return 0;
 }
 
@@ -98,4 +122,90 @@ void clearbit(short* x,int i){
 
 int isbitset(short* x,int i){
   return (*x|(1<<i))==*x;
+}
+
+int copyPLUScrc(char *source, char *target)
+{
+    int fd_source, fd_target, tmp;
+    int returnval = COPY_ERROR;
+
+    // open source
+    fd_source = open(source, O_RDONLY);
+    if(fd_source == -1){
+        returnval = COPY_NO_SOURCE;
+    } else {
+        // open target
+        fd_target = open(target, O_APPEND | O_CREAT | O_EXCL | O_WRONLY, S_IRWXU);
+        if ( fd_target == -1){
+            returnval = errno==EEXIST ? COPY_FILE_EXISTS : COPY_ERROR;
+        } else {
+            // copy
+            while(1){
+                tmp = read(fd_source, copy_buffer, BUFSIZE);
+                if ( tmp == 0 ){ // EOF
+                    returnval = COPY_SUCCESS;
+		    write(fd_target,crc,2);
+                    break;
+                } else if ( tmp == -1 ) { // ERROR
+                    break;
+                } else {
+                    // write
+                    if ( write(fd_target, copy_buffer, tmp) == -1 ){
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    // close
+    close(fd_target);
+    close(fd_source);
+    return returnval;
+}
+
+int copyMINUScrc(char *source, char *target)
+{
+    int fd_source, fd_target, tmp;
+    int returnval = COPY_ERROR;
+
+    // open source
+    fd_source = open(source, O_RDONLY);
+
+    //fseek((FILE *) fd_source, 0, SEEK_END);
+    //int sz = ftell((FILE *) fd_source);
+    //fseek((FILE *)fd_source, 0, SEEK_SET);
+
+    //printf("%i\n",sz);
+    
+    if(fd_source == -1){
+        returnval = COPY_NO_SOURCE;
+    } else {
+        // open target
+        fd_target = open(target, O_APPEND | O_CREAT | O_EXCL | O_WRONLY, S_IRWXU);
+        if ( fd_target == -1){
+            returnval = errno==EEXIST ? COPY_FILE_EXISTS : COPY_ERROR;
+        } else {
+            // copy
+            while(1){
+                tmp = read(fd_source, copy_buffer, BUFSIZE);
+                if ( tmp == 0 ){ // EOF
+                    returnval = COPY_SUCCESS;
+                    break;
+                } else if ( tmp == -1 ) { // ERROR
+                    break;
+                } else {
+                    // write
+                    if ( write(fd_target, copy_buffer, tmp) == -1 ){
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    // close
+    close(fd_target);
+    close(fd_source);
+    return returnval;
 }
